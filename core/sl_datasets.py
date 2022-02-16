f83872c9cf0db9090479ca4e7f11a528f7a7e441
terryxu <513848087@qq.com>
2022-02-16 22:33:55 +0000

    add SL dataset

--- core/sl_datasets.py
+++ core/sl_datasets.py
@@ -222,11 +222,9 @@ def fetch_dataloader(args):
     new_dataset = StructLight(aug_params)
     logging.info(f"Adding {len(new_dataset)} samples from Alister's Dataset")
     train_dataset = new_dataset
-    return train_dataset
+    
+    train_loader = data.DataLoader(train_dataset, batch_size=args.batch_size, 
+        pin_memory=True, shuffle=True, num_workers=int(os.environ.get('SLURM_CPUS_PER_TASK', 6))-2, drop_last=True)
 
-    # train_loader = data.DataLoader(train_dataset, batch_size=args.batch_size, 
-    #     pin_memory=True, shuffle=True, num_workers=int(os.environ.get('SLURM_CPUS_PER_TASK', 6))-2, drop_last=True)
-
-    # logging.info('Training with %d image pairs' % len(train_dataset))
-    # return train_loader
-
+    logging.info('Training with %d image pairs' % len(train_dataset))
+    return train_loader
